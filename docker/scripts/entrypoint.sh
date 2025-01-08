e266f84da72447eb5cf667fedb1b88ed3daa50d8
Cory Sanin <corysanin@outlook.com>
2025-01-08 00:59:40 -0500

    initial commit

---
+++ docker/scripts/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ -n "$DEP" ]
+then
+    if [ "$DEP" = "testing" ]
+    then
+        /scripts/testing.sh
+    fi
+fi
+
+if [ -z "$REPO" ]
+then
+    /bin/bash
+    exit $?;
+fi
+
+# Clone the git repo
+git clone "$REPO" /home/user/pkg
+cd /home/user/pkg
+makepkg -smf --noconfirm --noprogressbar --skippgpcheck --noarchive
+exit $?;
